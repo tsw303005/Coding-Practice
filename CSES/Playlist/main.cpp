fccbac7870970795eef038188c915d5a5561958e
tsw303005 <tsw303005@gmail.com>
2022-01-22 12:57:10 +0800

    feature: cses playlist

--- CSES/Playlist/main.cpp
+++ CSES/Playlist/main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
  
 using namespace std;
  
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    map<int, int> s;
+    unordered_map<int, int> s;
     int n, t, max, left;
  
     cin >> n;
     max = 0;
     left = 0;
+    s.rehash(n);
  
     for (int i = 1; i <= n; i++) {
         cin >> t;
-        if (s.find(t) != s.end() && s[t] > left) {
+        if (s[t] > left) {
             left = s[t];
         }
         s[t] = i;
